730a06fadb79a5dae4e953ba35089f5f08b260c5
Sajan Gutta <gutta@usc.edu>
2020-10-26 21:20:15 -0700

    handling strings default case, only doing default for primitive types

--- peewee.py
+++ peewee.py
@@ -4463,7 +4463,10 @@ class Field(ColumnBase):
         if self.constraints:
             accum.extend(self.constraints)
         if self.default is not None:
-            accum.append(SQL("DEFAULT %s" % self.default))
+            if isinstance(self.default, str):
+                accum.append(SQL("DEFAULT '%s'" % self.default))
+            elif isinstance(self.default, (bool, int)):
+                accum.append(SQL("DEFAULT %s" % self.default))
         if self.collation:
             accum.append(SQL('COLLATE %s' % self.collation))
         return NodeList(accum)
